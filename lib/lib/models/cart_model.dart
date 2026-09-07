9b7b413ca38f0840eb9cbfbab021f2fd1e269bf6
sangram3304-png <sangram3304@gmail.com>
2026-09-07 21:19:56 +0530

    Add CartModel to manage food items in cart

--- lib/lib/models/cart_model.dart
+++ lib/lib/models/cart_model.dart
@@ -1,1 +1,25 @@
+import 'food_model.dart';
 
+class CartModel {
+  final FoodModel food;
+  int quantity;
+
+  CartModel({
+    required this.food,
+    this.quantity = 1,
+  });
+
+  double get totalPrice => food.price * quantity;
+
+  Map<String, dynamic> toMap() {
+    return {
+      'foodId': food.id,
+      'restaurantId': food.restaurantId,
+      'name': food.name,
+      'price': food.price,
+      'category': food.category,
+      'quantity': quantity,
+      'totalPrice': totalPrice,
+    };
+  }
+}
